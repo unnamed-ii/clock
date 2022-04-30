9b8ff86f56d75474bc10c360b0309c12c67615de
iIngashev <isaingashev@gmail.com>
2022-05-01 00:49:47 +0300

    added functionality

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <div className="clock">
-          <div className="hour">
-              <div className="hr" id="hr">
-              </div>
-          </div>
-          <div className="min">
-              <div className="mn" id="mn">
-              </div>
-          </div>
-          <div className="sec">
-              <div className="sc" id="sc">
-              </div>
-          </div>
-      </div>
-    </>
-  );
+
+    const deg = 6;
+    const hour = useRef(null)
+    const minute = useRef(null)
+    const second = useRef(null)
+
+    useEffect(() => {
+        setInterval(() => {
+            let day = new Date();
+            let hh = day.getHours() * 30;
+            let mm = day.getMinutes() * deg;
+            let ss = day.getSeconds() * deg;
+
+            hour.current.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`;
+            minute.current.style.transform = `rotateZ(${mm}deg)`;
+            second.current.style.transform = `rotateZ(${ss}deg)`;
+        }, 1000)
+    }, [])
+
+    return (
+        <>
+            <div className="clock">
+                <div className="hour">
+                    <div className="hr" id="hr" ref={hour}>
+                    </div>
+                </div>
+                <div className="min">
+                    <div className="mn" id="mn" ref={minute}>
+                    </div>
+                </div>
+                <div className="sec">
+                    <div className="sc" id="sc" ref={second}>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
