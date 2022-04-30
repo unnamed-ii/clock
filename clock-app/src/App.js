1fe47b958a5fb1245d0131f1a038f80a8eab2d83
iIngashev <isaingashev@gmail.com>
2022-05-01 00:05:49 +0300

    added styles

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="clock">
+          <div className="hour">
+              <div className="hr" id="hr">
+              </div>
+          </div>
+          <div className="min">
+              <div className="mn" id="mn">
+              </div>
+          </div>
+          <div className="sec">
+              <div className="sc" id="sc">
+              </div>
+          </div>
+      </div>
+    </>
   );
 }
 
